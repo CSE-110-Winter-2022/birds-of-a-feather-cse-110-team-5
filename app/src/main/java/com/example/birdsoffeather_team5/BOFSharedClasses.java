bfc4c74395f2e1c112c950101e23dd8d5f5ea6ed
dparekhucsd <dparekh@ucsd.edu>
2022-02-06 15:37:14 -0800

    Created SharedStudents interface and class to implement it. Used for finding classes shared among two students

---
+++ app/src/main/java/com/example/birdsoffeather_team5/BOFSharedClasses.java
@@ -0,0 +1,36 @@
+package com.example.birdsoffeather_team5;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class BOFSharedClasses implements SharedClasses{
+    private Student mainStudent;
+    private Student otherStudent;
+    private List<ClassData> sharedClasses;
+
+    public void SharedClasses(Student mainStudent, Student otherStudent) {
+        this.mainStudent = mainStudent;
+        this.otherStudent = otherStudent;
+
+        sharedClasses = findSharedClasses(mainStudent, otherStudent);
+    }
+
+    public static List<ClassData> findSharedClasses(Student s1, Student s2) {
+        List<ClassData> shared = new ArrayList<>(s1.getClassData());
+
+        shared.retainAll(s2.getClassData());
+        return shared;
+    }
+
+    @Override
+    public List<ClassData> getSharedClasses() {
+        return sharedClasses;
+    }
+
+    public Student getMainStudent() {
+        return mainStudent;
+    }
+    public Student getOtherStudent() {
+        return otherStudent;
+    }
+}
